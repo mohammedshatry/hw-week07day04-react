4e2a8ce69d3a72d4f8e0e3650fd0802fc05a0a9b
mohammedshatry <mohammedshatry@hotmail.com>
2020-01-01 17:59:07 +0300

    add react app

---
+++ hw-w7d4/src/Adder.js
@@ -0,0 +1,14 @@
+import React from 'react'; //JSX
+const num1=55;
+const num2=100;
+const numsum=num1+num2;
+function Adder() {
+    return(
+        <div>
+        <h1>The sum of {num1} and {num1} is {numsum}</h1>
+        
+        </div>
+    )
+  }
+
+  export default Adder;
